8bac97b47c91428f2585164919256805d0fc0c31
Marcel Valdez <marcel.valdez@live.com>
2011-11-27 17:34:40 -0600

    + Pasa los tests de serialización y deserialización

--- test/data/structs/ForeignMasterRecordTest.java
+++ test/data/structs/ForeignMasterRecordTest.java
@@ -7,6 +7,9 @@ package data.structs;
 import data.Octet;
 import data.UInt16;
 import data.UInt8;
+import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -18,8 +21,8 @@ import static org.junit.Assert.*;
  *
  * @author Marcel
  */
-public class ForeignMasterRecordTest {
-    
+public class ForeignMasterRecordTest extends DataStructTest {
+
     public ForeignMasterRecordTest() {
     }
 
@@ -30,11 +33,11 @@ public class ForeignMasterRecordTest {
     @AfterClass
     public static void tearDownClass() throws Exception {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -42,16 +45,67 @@ public class ForeignMasterRecordTest {
     @Test
     public void testSerializeDeserialize() {
         ForeignMasterRecord expected = new ForeignMasterRecord();
-        expected.foreign_master_communication_technology = new UInt8((byte)0);
-        expected.foreign_master_port_id = new UInt16((byte)1, (byte)2);
-        expected.foreign_master_syncs = new UInt16((byte)3, (byte)4);
-        expected.foreign_master_uuid[0] = new Octet((byte)5);
-        expected.foreign_master_uuid[1] = new Octet((byte)6);
-        expected.foreign_master_uuid[2] = new Octet((byte)7);
-        expected.foreign_master_uuid[3] = new Octet((byte)8);
-        expected.foreign_master_uuid[4] = new Octet((byte)9);
-        expected.foreign_master_uuid[5] = new Octet((byte)10);
-        expected.header = new MsgHeader();
-                
+        expected.foreign_master_communication_technology = new UInt8((byte) 0);
+        expected.foreign_master_port_id = new UInt16((byte) 1, (byte) 2);
+        expected.foreign_master_syncs = new UInt16((byte) 3, (byte) 4);
+        expected.foreign_master_uuid[0] = new Octet((byte) 5);
+        expected.foreign_master_uuid[1] = new Octet((byte) 6);
+        expected.foreign_master_uuid[2] = new Octet((byte) 7);
+        expected.foreign_master_uuid[3] = new Octet((byte) 8);
+        expected.foreign_master_uuid[4] = new Octet((byte) 9);
+        expected.foreign_master_uuid[5] = new Octet((byte) 10);
+        MsgHeader header = new MsgHeader();
+        header.control = new UInt8((byte) 1);
+        header.flags[0] = new Octet((byte) 2);
+        header.flags[1] = new Octet((byte) 3);
+        header.messageType = new UInt8((byte) 4);
+        header.sequenceId = new UInt16((byte) 5, (byte) 6);
+        header.sourceCommunicationTechnology = new UInt8((byte) 7);
+        header.sourcePortId = new UInt16((byte) 8, (byte) 9);
+        header.sourceUuid[0] = new Octet((byte) 10);
+        header.sourceUuid[1] = new Octet((byte) 11);
+        header.sourceUuid[2] = new Octet((byte) 12);
+        header.sourceUuid[3] = new Octet((byte) 13);
+        header.sourceUuid[4] = new Octet((byte) 14);
+        header.sourceUuid[5] = new Octet((byte) 15);
+        header.subdomain[0] = new Octet((byte) 16);
+        header.subdomain[1] = new Octet((byte) 17);
+        header.subdomain[2] = new Octet((byte) 18);
+        header.subdomain[3] = new Octet((byte) 19);
+        header.subdomain[4] = new Octet((byte) 20);
+        header.subdomain[5] = new Octet((byte) 21);
+        header.subdomain[6] = new Octet((byte) 22);
+        header.subdomain[7] = new Octet((byte) 23);
+        header.subdomain[8] = new Octet((byte) 24);
+        header.subdomain[9] = new Octet((byte) 25);
+        header.subdomain[10] = new Octet((byte) 26);
+        header.subdomain[11] = new Octet((byte) 27);
+        header.subdomain[12] = new Octet((byte) 28);
+        header.subdomain[13] = new Octet((byte) 29);
+        header.subdomain[14] = new Octet((byte) 30);
+        header.subdomain[15] = new Octet((byte) 31);
+        expected.header = header;
+        expected.sync = new MsgSync();
+
+        byte[] data = expected.serialize();
+        ForeignMasterRecord actual = new ForeignMasterRecord();
+
+        try {
+            actual.load(data);
+            this.AssertStructsAreEqual(actual, expected);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(ForeignMasterRecordTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(ForeignMasterRecordTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            Logger.getLogger(ForeignMasterRecordTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoSuchMethodException ex) {
+            Logger.getLogger(ForeignMasterRecordTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(ForeignMasterRecordTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvocationTargetException ex) {
+            Logger.getLogger(ForeignMasterRecordTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 }
